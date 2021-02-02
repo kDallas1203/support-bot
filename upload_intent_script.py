fc393763f61e9c2995c735e25eecf79872bc2504
Ilya <ilya.teterich@yandex.ru>
2021-02-02 11:57:41 +0300

    Refactor upload_intent_script.py

--- upload_intent_script.py
+++ upload_intent_script.py
@@ -3,6 +3,7 @@ import json
 import logging
 from google.cloud import dialogflow
 
+PROJECT_ID = os.environ.get('DIALOG_FLOW_PROJECT_ID')
 
 def upload_intent(project_id, intent):
     intents_client = dialogflow.IntentsClient()
@@ -47,7 +48,6 @@ def get_intents(questions):
 
 
 def main():
-    project_id = os.environ.get('DIALOG_FLOW_PROJECT_ID')
     logging.basicConfig(level=logging.INFO)
     try:
         with open("questions.json", "r") as question_json:
@@ -56,7 +56,7 @@ def main():
         intents = get_intents(questions)
 
         for intent in intents:
-            upload_intent(project_id, intent)
+            upload_intent(PROJECT_ID, intent)
 
     except FileNotFoundError:
         logging.error('File not found', exc_info=True)
